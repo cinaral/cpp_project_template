dda09d1ab8bc52270f6145c673c65f0683cc3e85
cinaral <alevent.cinar@gmail.com>
2022-11-10 16:37:32 -0600

    Add cmake bash scripts

---
+++ scripts/cmake/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#*  change the cwd to the script dir temporarily, and hide pushd popd output
+pushd () { 
+	command pushd "$@" > /dev/null 
+}
+popd () { 
+	command popd "$@" > /dev/null 
+}
+pushd "$(dirname ${BASH_SOURCE:0})"
+trap popd EXIT
+
+ctest --test-dir ../../build -t test
+
+echo "$0 done."
